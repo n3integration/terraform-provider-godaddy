6afa585214b38bfe117cda934dc7e03d3ba8568c
n3integration <n3integration@gmail.com>
2017-01-04 22:00:23 -0500

    cleanup installation script [skip ci]

--- install.sh
+++ install.sh
@@ -1,27 +1,51 @@
 #!/bin/sh
 
-version=0.0.2
+version=1.0.0
+
 os=$(uname -s | tr '[:upper:]' '[:lower:]')
+mach=$(uname -m)
+plugin="terraform-godaddy"
 
 case "$os" in
   "linux"|"darwin")
     printf "[info] fetching latest revision..."
-    curl -fsSL https://github.com/n3integration/terraform-godaddy/releases/download/v$version/terraform-godaddy-$os-$version.tgz | gzip -dc | tar xf -
 
-    if [ -f terraform-godaddy ]; then
+    if [ "$mach" == "x86_64" ]; then
+      arch="amd64"
+    else
+      arch="386"
+    fi
+
+    file=$(printf "%s_%s_%s" $plugin $os $arch)
+    archive="$file.tgz"
+
+    cd /tmp && curl -fOsSL https://github.com/n3integration/$plugin/releases/download/v$version/$archive 2>/dev/null
+
+    if [ ! -f /tmp/$archive ]; then
+      echo "ERROR"
+      echo "\t-> failed to download file"
+      exit 1
+    else 
+      cd /tmp && gzip -dc $archive | tar xf -
+      mv $file $plugin
+    fi
+
+    if [ -f $plugin ]; then
       echo "OK"
     else
+      echo "ERROR"
+      echo "\t-> failed to extract file contents"
       exit 1
     fi
     ;;
   *)
-    echo "[error] Whoops. It doesn't look like your OS is currently supported."
+    echo "[error] Dagger. It doesn't look like your OS is currently supported. Please submit an issue or pull request."
     exit 1
     ;;
 esac
 
 printf "[info] installing plugin..."
-mkdir -p ~/.terraform/plugins && mv terraform-godaddy ~/.terraform/plugins
+mkdir -p ~/.terraform/plugins && mv $plugin ~/.terraform/plugins
 if [ $? -eq 0 ]; then
   echo "OK"
 else
@@ -35,14 +59,14 @@ if [ -f ~/.terraformrc ]; then
     echo "[info] append the godaddy provider to your ~/.terraformrc configuration file"
     echo ""
     echo "providers {"
-    echo "\tgodaddy = \"$HOME/.terraform/plugins/terraform-godaddy\""
+    echo "\tgodaddy = \"$HOME/.terraform/plugins/$plugin\""
     echo "}"
     echo ""
   fi
 else
   cat > ~/.terraformrc <<EOF
 providers {
-  godaddy = "$HOME/.terraform/plugins/terraform-godaddy"
+  godaddy = "$HOME/.terraform/plugins/$plugin"
 }
 EOF
   echo "[info] complete"
