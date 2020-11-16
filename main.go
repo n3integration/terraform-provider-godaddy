be119630f9c93cd4c5a1a70b810d04f9640998d9
kolikons <kolikons@gmail.com>
2020-11-16 17:58:15 +0200

    fix main

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+
+	"terraform-provider-godaddy/plugin/terrafrom-godaddy"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return hashicups.Provider()
+		},
+	})
+}
